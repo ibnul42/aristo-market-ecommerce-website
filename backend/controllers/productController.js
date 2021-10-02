e0aa8b52f0843e4461a5dd528aaff4ef2875de9c
Ibnul Ashir <ibnulashir42@gmail.com>
2021-10-02 16:20:34 +0600

    serach, filter, pagination added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,6 +2,7 @@ const Product = require('../models/Product');
 
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const APIFeatures = require('../utils/apiFeatures');
 
 // create new product => /api/v1/admin/product/new
 exports.newProduct = catchAsyncErrors(async (req, res, next) => {
@@ -14,12 +15,21 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 })
 
 
-// get all products => /api/v1/products 
+// get all products => /api/v1/products?keywords=name
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
-    const products = await Product.find();
+
+    const resPerPage = 2;
+    const productCount = await Product.countDocuments();
+
+    const apiFeatures = new APIFeatures(Product.find(), req.query)
+                        .search()
+                        .filter()
+                        .pagination(resPerPage)
+    const products = await apiFeatures.query;
     res.status(200).json({
         success: true,
         count: products.length,
+        productCount,
         products
     }) 
 })
